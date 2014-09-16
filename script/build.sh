42c86689d9b9106e576bc796dfe856ebbe35da71
teanet <teanet@yandex.ru>
2014-09-16 16:58:52 +0700

    add build script

---
+++ script/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+GIT_DIR=$(dirname $(pwd))
+PROJECT_DIR="$GIT_DIR/iOS"
+
+cd $GIT_DIR
+echo "pull repo..."
+git pull origin master
+
+type pod >/dev/null 2>&1 || { echo >&2 "I require CocoaPods but it's not installed. Aborting."; exit 1; }
+
+cd $PROJECT_DIR
+echo "install Pods..."
+pod install  2>&1  || { echo "pods doesn't install prorerly. Aborting."; exit 1;}
+
+echo "check XCode..."
+type xcodebuild >/dev/null 2>&1 || { echo >&2 "I require XCode but it's not installed.  Aborting."; exit 1; }
+
+echo "build project..."
+
+workspace="$PROJECT_DIR/Omnom.xcworkspace"
+
+xcodebuild test \
+  -workspace $workspace \
+  -scheme "omnom" \
+  -configuration "Test" \
+  -destination 'platform=iOS Simulator,name=iPhone Retina (4-inch),OS=7.1' \
+  -verbose
