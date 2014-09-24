904afc5b48235e86dddd399c665108205ffd0b74
teanet <teanet@yandex.ru>
2014-09-24 11:14:17 +0700

    fix #135: в релизе 2476 переписал весь экран

--- iOS/Omnom/Omnom/Controllers/OMNCLPermissionsHelpVC.m
+++ iOS/Omnom/Omnom/Controllers/OMNCLPermissionsHelpVC.m
@@ -9,6 +9,7 @@
 #import "OMNCLPermissionsHelpVC.h"
 #import "OMNScrollExtendView.h"
 #import "OMNUtils.h"
+#import "OMNConstants.h"
 
 @interface OMNCLPermissionsHelpVC ()
 <UIScrollViewDelegate>
@@ -17,46 +18,34 @@
 
 @implementation OMNCLPermissionsHelpVC {
   
-  __weak IBOutlet UILabel *_label;
-  __weak IBOutlet UIPageControl *_pageControl;
-  __weak IBOutlet UIScrollView *_scrollView;
+  UIView *_contentView;
+  UILabel *_label;
+  UIPageControl *_pageControl;
+  UIScrollView *_scrollView;
   NSArray *_labelTexts;
+
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-  if (self) {
-    self.backgroundImage = [UIImage imageNamed:@"wood_bg"];
-//    self.buttonInfo =
-//    @[
-//      @{
-//        @"title" : NSLocalizedString(@"Повторить", nil),
-//        @"image" : [UIImage imageNamed:@"repeat_icon_small"],
-//        @"block" : ^{
-//          
-//          [weakSelf.navigationController popToViewController:weakSelf animated:YES];
-//          
-//        },
-//        }
-//      ];
-    
-  }
-  return self;
+- (void)loadView {
+  self.view = [[OMNScrollExtendView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
+  self.view.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
 }
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   
+  self.backgroundImage = [UIImage imageNamed:@"wood_bg"];
+  
+  [self setup];
+
   OMNScrollExtendView *scrollExtendView = (OMNScrollExtendView *)self.view;
   if ([scrollExtendView respondsToSelector:@selector(setScrollView:)]) {
     scrollExtendView.scrollView = _scrollView;
   }
   
   _label.textColor = [UIColor blackColor];
-  _label.font = [UIFont fontWithName:@"Futura-OSF-Omnom-Regular" size:25.0f];
+  _label.font = FuturaOSFOmnomRegular(25.0f);
   
-  NSInteger pagesCount = 3;
-
   _pageControl.pageIndicatorTintColor = [UIColor lightGrayColor];
   _pageControl.currentPageIndicatorTintColor = [UIColor blackColor];
 
@@ -66,7 +55,7 @@
     _labelTexts =
     @[
       NSLocalizedString(@"Чтобы разрешить геолокацию, откройте список приватности:", nil),
-      NSLocalizedString(@"Затем откройте службы геолокации:", nil),
+      NSLocalizedString(@"Затем откройте\nСлужбы геолокации:", nil),
       NSLocalizedString(@"Разрешите, наконец, использовать службы геолокации для Omnom:", nil),
       ];
   } else {
@@ -74,29 +63,101 @@
     _labelTexts =
     @[
       NSLocalizedString(@"Чтобы разрешить геолокацию, откройте Конфиденциальность:", nil),
-      NSLocalizedString(@"Затем откройте службы геолокации:", nil),
+      NSLocalizedString(@"Затем откройте\nСлужбы геолокации:", nil),
       NSLocalizedString(@"Разрешите доступ к геопозиции:", nil),
       ];
   }
+
+  [self setPage:0];
   
-  CGRect frame = _scrollView.bounds;
-  CGSize contentSize = CGSizeMake(frame.size.width*pagesCount, frame.size.height);
-  _scrollView.contentSize = contentSize;
+}
+
+- (void)setup {
+  
+  NSInteger pagesCount = 3;
+  
+  _scrollView = [[UIScrollView alloc] init];
+  _scrollView.pagingEnabled = YES;
+  _scrollView.bounces = NO;
+  _scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.view addSubview:_scrollView];
+  
+  _label = [[UILabel alloc] init];
+  _label.numberOfLines = 0;
+  _label.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.view addSubview:_label];
+  
+  _pageControl = [[UIPageControl alloc] init];
+  _pageControl.translatesAutoresizingMaskIntoConstraints = NO;
+  _pageControl.numberOfPages = pagesCount;
+  [self.view addSubview:_pageControl];
+  
+  _contentView = [[UIView alloc] init];
+  _contentView.translatesAutoresizingMaskIntoConstraints = NO;
+  [_scrollView addSubview:_contentView];
+  
+  NSMutableDictionary *views =
+  [@{
+    @"scrollView" : _scrollView,
+    @"label" : _label,
+    @"pageControl" : _pageControl,
+    @"contentView" : _contentView,
+    @"topLayoutGuide" : self.topLayoutGuide,
+    } mutableCopy];
+  
+  CGSize size = CGSizeZero;
   
   for (int i = 0; i < pagesCount; i++) {
     
-    frame.origin.x = frame.size.width*i;
-    UIImageView *iv = [[UIImageView alloc] initWithFrame:frame];
+    UIImageView *iv = [[UIImageView alloc] init];
+    iv.translatesAutoresizingMaskIntoConstraints = NO;
     if (SYSTEM_VERSION_LESS_THAN(@"8.0")) {
       iv.image = [UIImage imageNamed:[NSString stringWithFormat:@"navigation_help_%d-7", i + 1]];
     } else {
       iv.image = [UIImage imageNamed:[NSString stringWithFormat:@"navigation_help_%d-8", i + 1]];
     }
-    [_scrollView addSubview:iv];
     
+    if (CGSizeEqualToSize(size, CGSizeZero)) {
+      size = iv.image.size;
+    }
+    [_contentView addSubview:iv];
+    views[[NSString stringWithFormat:@"iv%d", i]] = iv;
   }
 
-  [self setPage:0];
+  NSDictionary *metrics =
+  @{
+    @"height" : @(size.height),
+    @"width" : @(size.width),
+    };
+  
+  [_contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[iv0][iv1][iv2]|" options:0 metrics:nil views:views]];
+  [_contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[iv0]|" options:0 metrics:nil views:views]];
+  [_contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[iv1]|" options:0 metrics:nil views:views]];
+  [_contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[iv2]|" options:0 metrics:nil views:views]];
+  
+  [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[contentView]|" options:0 metrics:metrics views:views]];
+  [_scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[contentView]|" options:0 metrics:metrics views:views]];
+  
+  [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[scrollView(width)]" options:0 metrics:metrics views:views]];
+  
+  [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_pageControl attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:_scrollView attribute:NSLayoutAttributeBottom multiplier:1.0f constant:20.0f]];
+  [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_pageControl attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:_scrollView attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f]];
+  
+  [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(>=20)-[label]-(>=20)-|" options:0 metrics:metrics views:views]];
+  [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_label attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f]];
+  [self.view addConstraint:[NSLayoutConstraint constraintWithItem:_scrollView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f]];
+  
+  [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(50)-[label(100)][scrollView(height)]" options:0 metrics:metrics views:views]];
+  
+}
+
+- (void)viewWillLayoutSubviews {
+  [super viewWillLayoutSubviews];
+  CGSize size = _scrollView.frame.size;
+  size.width *= 3;
+  _scrollView.contentSize = size;
+
+  NSLog(@"%@", _pageControl);
   
 }
 
