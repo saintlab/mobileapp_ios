ae04ab3a07d678c92cf2753e4a436ac14c5b22d0
teanet <teanet@yandex.ru>
2014-12-25 14:55:20 +0600

    #453

---
+++ iOS/Omnom/Omnom/Controllers/Restaurant/OMNRestaurantCardVC.h
@@ -0,0 +1,13 @@
+//
+//  OMNRestaurantCardVC.h
+//  omnom
+//
+//  Created by tea on 25.12.14.
+//  Copyright (c) 2014 tea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OMNRestaurantCardVC : UIViewController
+
+@end
---
+++ iOS/Omnom/Omnom/Controllers/Restaurant/OMNRestaurantCardVC.m
@@ -0,0 +1,13 @@
+//
+//  OMNRestaurantCardVC.m
+//  omnom
+//
+//  Created by tea on 25.12.14.
+//  Copyright (c) 2014 tea. All rights reserved.
+//
+
+#import "OMNRestaurantCardVC.h"
+
+@implementation OMNRestaurantCardVC
+
+@end
