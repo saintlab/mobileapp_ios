4dc95588c1adf41a5c7b4423e345e11a8c6076ca
teanet <teanet@yandex.ru>
2015-01-19 16:13:58 +0600

    change dependences

---
+++ iOS/Omnom/Omnom/Controllers/Preorder/OMNPreorderConfirmVC.h
@@ -0,0 +1,13 @@
+//
+//  OMNPreorderConfirmVC.h
+//  omnom
+//
+//  Created by tea on 19.01.15.
+//  Copyright (c) 2015 tea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OMNPreorderConfirmVC : UITableViewController
+
+@end
