810967ca5431fec06165d77f18f2cdbbd6b94e10
teanet <teanet@yandex.ru>
2015-01-30 16:24:12 +0600

    fix #545:в релизе 2535

--- iOS/Omnom/Omnom/Model/Order/OMNOrderTansactionInfo.m
+++ iOS/Omnom/Omnom/Model/Order/OMNOrderTansactionInfo.m
@@ -21,18 +21,7 @@
     _bill_amount = order.enteredAmount;
     _tips_amount = order.tipAmount;
     _total_amount = order.enteredAmountWithTips;
-    
-    if (_bill_amount) {
-      
-      _tips_percent = (double)_tips_amount/_bill_amount;
-      
-    }
-    else {
-      
-      _tips_percent = 0.0;
-      
-    }
-    
+    _tips_percent = (_bill_amount) ? (100.0*((double)_tips_amount/_bill_amount)) : (0.0);
     _tips_way = stringFromTipType(order.tipType);
     _split = stringFromSplitType(order.splitType);
     
