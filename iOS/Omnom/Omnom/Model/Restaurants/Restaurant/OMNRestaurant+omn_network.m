466a96df3776932baaaf1a60ae25e9c3bd22140e
teanet <teanet@yandex.ru>
2015-02-10 16:54:01 +0600

    remove demo mode

--- iOS/Omnom/Omnom/Model/Restaurants/Restaurant/OMNRestaurant+omn_network.m
+++ iOS/Omnom/Omnom/Model/Restaurants/Restaurant/OMNRestaurant+omn_network.m
@@ -104,9 +104,6 @@
     return NO;
   }
 
-#warning readyForPush
-  return YES;
-  
   BOOL readyForPush = YES;
   const NSTimeInterval timeIntervalForLocalPush = 4.0*60.0*60.0;
   NSDate *lastPushDate = [self lastPushDate];
@@ -124,8 +121,6 @@
 
 - (BOOL)readyForEnter {
   
-#warning 123
-  return YES;
   BOOL readyForEnter = YES;
   const NSTimeInterval timeIntervalForEnter = 20.0*60.0;
   NSDate *lastEnterDate = [self lastEnterDate];
