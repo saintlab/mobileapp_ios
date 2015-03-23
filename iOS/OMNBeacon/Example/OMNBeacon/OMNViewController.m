d72ba80ae3161f2c67ad4138368fd3e811f9088d
teanet <teanet@yandex.ru>
2015-03-23 14:39:42 +0600

    add beacon logic

--- iOS/OMNBeacon/Example/OMNBeacon/OMNViewController.m
+++ iOS/OMNBeacon/Example/OMNBeacon/OMNViewController.m
@@ -7,16 +7,43 @@
 //
 
 #import "OMNViewController.h"
+#import <OMNBeaconRangingManager.h>
+#import <OMNBeacon.h>
 
 @interface OMNViewController ()
 
 @end
 
-@implementation OMNViewController
+@implementation OMNViewController {
+  
+  OMNBeaconRangingManager *_beaconRangingManager;
+  CLLocationManager *_cl;
+  
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+  
+  if (kCLAuthorizationStatusNotDetermined == [CLLocationManager authorizationStatus]) {
+    _cl = [[CLLocationManager alloc] init];
+    [_cl requestAlwaysAuthorization];
+  }
+  
+  id jsonData = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"config.json" ofType:nil]] options:0 error:nil];
+  [OMNBeacon setBaeconUUID:[[OMNBeaconUUID alloc] initWithJsonData:jsonData[@"uuid"]]];
+  _beaconRangingManager = [[OMNBeaconRangingManager alloc] initWithStatusBlock:^(CLAuthorizationStatus status) {
+    
+  }];
+  
+  [_beaconRangingManager rangeBeacons:^(NSArray *beacons) {
+    
+    NSLog(@"%@", beacons);
+    
+  } failure:^(NSError *error) {
+    
+  }];
+  
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
